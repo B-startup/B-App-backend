a43c41576bf5df6c6a79a99560810dbdc62a912d
TuniCoder <sofiennemrabet16@gmail.com>
2025-07-30 17:18:58 +0100

    fix: Remove unused NotFoundException import in TagService
    - Clean up unused import to resolve linting warning
    - All 123 tests still passing in TagsMetadata modules

--- src/modules/TagsMetadata/tag/tag.service.ts
+++ src/modules/TagsMetadata/tag/tag.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, ConflictException, NotFoundException } from '@nestjs/common';
+import { Injectable, ConflictException } from '@nestjs/common';
 import { Tag } from '@prisma/client';
 import { PrismaService } from '../../../core/services/prisma.service';
 import { BaseService } from '../../../core/services/base.service';
