bb8e311a1d4b661a29fa752a78cb7b1a501a1aac
hajji saker <62725567+sakerhajji@users.noreply.github.com>
2025-07-31 13:53:07 +0100

    blocks cruds done without testsFile

--- src/modules/UserAccess/block/block.module.ts
+++ src/modules/UserAccess/block/block.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BlockService } from './block.service';
 import { BlockController } from './block.controller';
+import { PrismaClient } from '@prisma/client';
 
 @Module({
     controllers: [BlockController],
-    providers: [BlockService]
+    providers: [BlockService, PrismaClient]
 })
 export class BlockModule {}
